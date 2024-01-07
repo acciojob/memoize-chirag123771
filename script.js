3875664a9b88a665aae6a49ffb8ad2180a732ef5
Chirag Agarwal <72219269+chirag123771@users.noreply.github.com>
2024-01-08 02:08:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 function memoize(callback, resolver = JSON.stringify) {
-   
+    const cache = new Map();
+
+  const memoized = (...args) => {
+    const key = resolver ? resolver(...args) : JSON.stringify(args);
+
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    const result = callback(...args);
+    cache.set(key, result);
+
+    return result;
+  };
+
+  memoized.clear = () => {
+    cache.clear();
+  };
+
+  memoized.delete = (...args) => {
+    const key = resolver ? resolver(...args) : JSON.stringify(args);
+    cache.delete(key);
+  };
+
+  memoized.has = (...args) => {
+    const key = resolver ? resolver(...args) : JSON.stringify(args);
+    return cache.has(key);
+  };
+
+  return memoized;
   }
   
   module.exports = memoize;
